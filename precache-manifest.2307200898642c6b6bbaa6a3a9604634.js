0bef5a7d3933c877961ba313244a6552c2dec245
Abhishekgowda KM <34267@ITCINFOTECH.COM>
2020-07-24 15:51:57 +0530

    Updates

---
+++ precache-manifest.2307200898642c6b6bbaa6a3a9604634.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357f407eae0f2b64642b3a403cb49b84",
+    "url": "/eventechs-website/index.html"
+  },
+  {
+    "revision": "be41b75ef3d4c433da37",
+    "url": "/eventechs-website/static/css/main.bb6deede.chunk.css"
+  },
+  {
+    "revision": "6c2e639eb92f61a6809e",
+    "url": "/eventechs-website/static/js/2.6e252994.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eventechs-website/static/js/2.6e252994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be41b75ef3d4c433da37",
+    "url": "/eventechs-website/static/js/main.6edbe72e.chunk.js"
+  },
+  {
+    "revision": "03de90d1856d33bbc756",
+    "url": "/eventechs-website/static/js/runtime-main.48dc5537.js"
+  },
+  {
+    "revision": "47b62b2188c9b8874ce8dcf526551958",
+    "url": "/eventechs-website/static/media/main-home-gallery.47b62b21.jpg"
+  },
+  {
+    "revision": "f8e773f54ec3446031bc3fbbdd59327a",
+    "url": "/eventechs-website/static/media/main-home-page.f8e773f5.jpg"
+  },
+  {
+    "revision": "6cf5882a014a49e19de017f0e1feb042",
+    "url": "/eventechs-website/static/media/main-home-service.6cf5882a.jpg"
+  }
+]);
